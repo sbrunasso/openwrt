de6de1dfa6a681a8da5e8f9eaad46a78908505b8
sbrunasso <sbrunasso@ucla.edu>
2023-11-24 09:06:52 -0700

    Update owrt-setup-19.07.sh
    Adding in Statistics and Wake on Lan

--- owrt-setup-19.07.sh
+++ owrt-setup-19.07.sh
@@ -13,7 +13,18 @@ opkg install luci-app-ddns luci-app-opkg
 opkg install luci-app-firewall luci-app-upnp
 opkg install luci-app-freifunk-diagnostics
 opkg install luci-app-https-dns-proxy luci-app-ntpc 
-opkg install luci-app-statistics luci-app-wol
 
+## Install the statstics modules 
+# Default Sets of modules 
+# Configuring collectd-mod-rrdtool.
+# Configuring collectd-mod-iwinfo.
+# Configuring collectd-mod-cpu.
+# Configuring collectd-mod-memory.
+# Configuring collectd-mod-interface.
+# Configuring collectd-mod-load.
+# Configuring collectd-mod-network.
+
+opkg install luci-app-statistics luci-app-wol
+opkg install collectd-mod-apache collectd-mod-conntrack collectd-mod-iptables collectd-mod-nginx collectd-mod-ntpd collectd-mod-ping
 
 
